95d25884f2dceabfba7cfe379b46c42d78fdf494
rafaribeiro2013 <rafaribeiro2013@gmail.com>
2026-03-25 17:11:18 -0300

    começo do arquivo de webscrapping

--- teste_selenium.py
+++ teste_selenium.py
@@ -10,5 +10,10 @@ sleep(2)
 navegador.maximize_window()
 navegador.get('https://cdp.anp.gov.br/ords/r/cdp_apex/consulta-dados-publicos-cdp/consulta-produção-por-poço')
 
+form_data = navegador.find_element(By.ID, 'P54_PERIODO')
+dropdown_tipo_ambiente = navegador.find_element(By.ID, 'P54_AMBIENTE')
+
+sleep(2)
+
 
 navegador.quit()
